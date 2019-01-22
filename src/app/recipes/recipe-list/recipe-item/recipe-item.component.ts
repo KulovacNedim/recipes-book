2ca8df813658b832a0213aee1b0830bdcdc86dbc
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-01-22 17:55:13 +0100

    Data binded

--- src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
+++ src/app/recipes/recipe-list/recipe-item/recipe-item.component.ts
@@ -1,17 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
-import { Recipe } from '../recipes.model';
-
 @Component({
   selector: 'app-recipe-item',
   templateUrl: './recipe-item.component.html',
   styleUrls: ['./recipe-item.component.css']
 })
 export class RecipeItemComponent implements OnInit {
-
-  recipes: Recipe[] = [
-    new Recipe('A Test Recipe', 'This is simply a test', 'https://hips.hearstapps.com/del.h-cdn.co/assets/18/11/2048x1024/landscape-1520957481-grilled-salmon-horizontal.jpg?resize=1200:*')
-  ];
 
   constructor() { }
 
