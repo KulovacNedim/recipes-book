03e2d97ca30999a14ae0c8346a765281e8890919
Kulovac Nedim <nedim.kulovac@gmail.com>
2019-03-19 23:07:37 +0100

    Passing ingredients from recipe to shopping list implemented

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,15 +1,36 @@
 import { Recipe } from './recipe-list/recipes.model';
-import { EventEmitter } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
+import { ShoppingListService } from '../shopping-list/shopping-list.service';
 
+@Injectable()
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
 
     private recipes: Recipe[] = [
-        new Recipe('A Test Recipe', 'This is simply a test', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlOtu_HfrswrSnQ_mTdQRu8HCHn1deN62_DiyxYHMuXjJ95mjOUw'),
-        new Recipe('A Test Recipe 2', 'This is simply a test 2', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTlOtu_HfrswrSnQ_mTdQRu8HCHn1deN62_DiyxYHMuXjJ95mjOUw')
+        new Recipe('Tasty Schnitzel',
+            'A super-tasty Schnitzel - just awsome!',
+            'https://i.ytimg.com/vi/mbgenXJgWLs/hqdefault.jpg',
+            [
+                new Ingredient('Meet', 1),
+                new Ingredient('Frech Fries', 20)
+            ]),
+        new Recipe('Big Fat Burger',
+            'What else you need to say?',
+            'https://aht.seriouseats.com/images/20100331-fatburger-primary.jpg',
+            [
+                new Ingredient('Buns', 2),
+                new Ingredient('Meat', 1)
+            ])
     ];
+
+    constructor(private slService: ShoppingListService) {}
 
     getRecipes() {
         return this.recipes.slice();
+    }
+
+    addIngredientsToShoppingList(ingredients: Ingredient[]) {
+        this.slService.addIngredients(ingredients);
     }
 }
