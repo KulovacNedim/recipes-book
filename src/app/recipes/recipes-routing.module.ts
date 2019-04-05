de895a9fc12c2d8c5f6ffa54b849c04e3d6ca6c3
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-04-05 14:24:46 +0200

    RecipeModule gets load lazy@

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -7,13 +7,13 @@ import { RecipeDetailsComponent } from './recipe-details/recipe-details.componen
 import { AuthGuard } from '../auth/guard.service';
 
 const recipesRoutes: Routes = [
-    { path: 'recipes', component: RecipesComponent, children: [
+    { path: '', component: RecipesComponent, children: [
         { path: '', component: RecipeStartComponent },
         { path: 'new', component: RecipeEditComponent, canActivate: [AuthGuard] },
         { path: ':id', component: RecipeDetailsComponent },
         { path: ':id/edit', component: RecipeEditComponent, canActivate: [AuthGuard] }
     ] },
-]
+];
 
 @NgModule({
     imports: [
