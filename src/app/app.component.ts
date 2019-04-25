3c7ced50177132fc0623512d226340b00d3d864e
Nedim Kulovac <nedim.kulovac@gmail.com>
2019-04-25 23:48:07 +0200

    Angular Universal rendering implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import * as firebase from 'firebase';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -17,7 +17,14 @@ export class AppComponent implements OnInit {
   ngOnInit() {
     firebase.initializeApp({
       apiKey: "AIzaSyCPlf5yxrQi8xyFARidVN_Qx4yHcRmtrQg",
-    authDomain: "ng-recipe-book-b01a5.firebaseapp.com"
+    authDomain: "ng-recipe-book-b01a5.firebaseapp.com",
+    databaseURL: "https://ng-recipe-book-b01a5.firebaseio.com",
+    projectId: "ng-recipe-book-b01a5",
+    storageBucket: "ng-recipe-book-b01a5.appspot.com",
+    messagingSenderId: "641466166385"
+
+      // apiKey: "AIzaSyCPlf5yxrQi8xyFARidVN_Qx4yHcRmtrQg",
+      // authDomain: "ng-recipe-book-b01a5.firebaseapp.com"
     });
   }
 }
